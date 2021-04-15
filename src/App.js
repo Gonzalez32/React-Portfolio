82f339767112ac04ba91806f0e74eed7b97e67c2
Gonzalez32 <3232rose@gmail.com>
2021-04-15 15:34:47 -0500

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar"
 
 function App() {
-  return <h1>This is a message</h1>;
+  return (
+    <Navbar/>
+  )
 }
 
 export default App;
+ 
