e6fa6e1ab77b3e6641dbf5cdef033fa63004cb3f
Gonzalez32 <3232rose@gmail.com>
2021-04-18 14:15:13 -0500

    added background

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
     return (
-        <div className="header-wrapper">
+        <div className="header-wraper">
             <div className="main-info">
                 <h1>Web Development and website needs</h1>
             </div>
