d8d4e17cbccf5ece4ed97832fbe516a366e7093d
Gonzalez32 <3232rose@gmail.com>
2021-04-19 11:53:35 -0500

    install react particles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particels from "react-particles-js"
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
+      <Particels
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: "#f9ab00"
+              }
+            }
+          }
+        }}
+      />
       <Navbar/>
       <Header/>
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Typed from "react-typed";
+
 
 const Header = () => {
     return (
         <div className="header-wraper">
             <div className="main-info">
-                <h1>Web Development and website needs</h1>
+                <h1>Web Development & website services</h1>
+                <Typed
+                    className="typed-text"
+                    strings={["Web Design", "Web Development", "SMM", "Clients Ads"]}
+                    typeSpeed={40}
+                    backSpeed={60}
+                    loop
+                />
+                <a href="#" className="btn-main-offer">contact me here</a>
             </div>
         </div>
     )
