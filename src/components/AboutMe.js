e6ba76ac25c5fec61143a7687f25141e983f4613
Gonzalez32 <3232rose@gmail.com>
2021-04-28 11:07:25 -0500

    modified App.css

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,10 +6,12 @@ const AboutMe = () => {
         <div className="container py-5">
             <div className="row">
                 <div className="col-lg-6 col-xm-12">
-                    <img src={author} className="img-thumbnail" alt="author..."/>
+                    <div className="photo-wrap">
+                        <img src={author} className="img-thumbnail-profile" alt="author..."/>
+                    </div>
                 </div>
                 <div className="col-lg-6 col-xm-12">
-                    <h1>About Me</h1>
+                    <h1 className="header-aboutme">About Me</h1>
                     <p>
                         Hello User, It's nice to meet you! Welcome to my Portfoilo, I have a dog named Ferins he's such a goofy boy that love's to play fetch.
                         My life has been a crazy turn of events. Here I stand with my certificate from GA as a Software Engineer. This Portfolio Is build with ReactJS, a powerful front-end framework.
