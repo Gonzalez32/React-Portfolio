6c29bb3b069907a0b86c6a70a3663091792d960a
Gonzalez32 <3232rose@gmail.com>
2021-05-03 15:26:53 -0500

    modified files for Exp

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -8,36 +8,36 @@ const Experience = () => {
             </div>
             <div className="container experience-wrapper">
                 <div className="timeline-block timeline-block-right">
-                    <div className="marker">
-                        <div className="timeline-content">
-                            <h3>2014-2015</h3>
-                            <p>lorem i
-                                here it goes
-                            </p>
-                        </div>
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2012-2014</h3>
+                        <p>
+                            lorem i here it goes more info will incress this message and should even out the layout hopefully.
+                            Also may the odds be in your most ever favor!
+                        </p>
                     </div>
-                    {/* {} */}
-                    <div className="timeline-block timeline-block-left">
-                        <div className="marker">
-                            <div className="timeline-content">
-                                <h3>2014-2015</h3>
-                                <p>lorem i
-                                    here it goes
-                                </p>
-                            </div>
-                        </div>
-                    </div>    
-                    {/* {} */}
-                    <div className="timeline-block timeline-block-right">
-                        <div className="marker">
-                            <div className="timeline-content">
-                                <h3>2014-2015</h3>
-                                <p>lorem i
-                                    here it goes
-                                </p>
-                            </div>
-                        </div>
-                    </div>    
+                </div>
+                {/* {} */}
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2014-2016</h3>
+                        <p>
+                            here it goes more info will incress this message and should even out the layout hopefully.
+                            Also may the odds be in your most ever favor!
+                        </p>
+                    </div>
+                </div>    
+                {/* {} */}
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div> 
+                    <div className="timeline-content">
+                        <h3>2017-2020</h3>
+                        <p>
+                            here it goes more info will incress this message and should even out the layout hopefully.
+                            Also may the odds be in your most ever favor!
+                        </p>
+                    </div> 
                 </div>
             </div>
         </div>
