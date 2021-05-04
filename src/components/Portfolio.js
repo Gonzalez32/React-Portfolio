98e671b9068d188e6af8e24c006bf903398c83d4
Gonzalez32 <3232rose@gmail.com>
2021-05-04 12:03:53 -0500

    added new component

---
+++ src/components/Portfolio.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Portfolio = () => {
+    return (
+        <div className="portfolio-wrapper">
+            <div className="container">
+                <h1 className="text-uppercase text-center py-5"></h1>
+                <div className="image-box-wrapper row justify-content-center">
+                    <img src="" alt="" className="portfolio-image"/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Portfolio;
