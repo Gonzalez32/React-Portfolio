af481dda124be2aaa68f3ab57d81bb3b5dd6de98
Gonzalez32 <3232rose@gmail.com>
2021-05-06 07:58:02 -0500

    added more content

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,8 @@
 import React from "react";
+import fallingstar from "../Image/fallingstar.png"
+import newpost from "../Image/newpost.png"
+import tapmate from "../Image/tapmate.png"
+
 
 const Portfolio = () => {
     return (
@@ -6,7 +10,15 @@ const Portfolio = () => {
             <div className="container">
                 <h1 className="text-uppercase text-center py-5"></h1>
                 <div className="image-box-wrapper row justify-content-center">
-                    <img src="" alt="" className="portfolio-image"/>
+                    <img src={fallingstar} alt="falling star project..." className="portfolio-image"/>
+                </div>
+                {/* - */}
+                <div className="image-box-wrapper row justify-content-center">
+                    <img src={newpost} alt="newpost project..." className="portfolio-image"/>
+                </div>
+                {/* - */}
+                <div className="image-box-wrapper row justify-content-center">
+                    <img src={tapmate} alt="tapmate project..." className="portfolio-image"/>
                 </div>
             </div>
         </div>
