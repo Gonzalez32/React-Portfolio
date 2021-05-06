e8ace295b0ae6bc4c0f110241b7a0117e7201a86
Gonzalez32 <3232rose@gmail.com>
2021-05-06 10:55:02 -0500

    modified App.css

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,17 +8,22 @@ const Portfolio = () => {
     return (
         <div className="portfolio-wrapper">
             <div className="container">
-                <h1 className="text-uppercase text-center py-5"></h1>
+                <h1 className="text-uppercase text-center py-5">Portfolio</h1>
                 <div className="image-box-wrapper row justify-content-center">
-                    <img src={fallingstar} alt="falling star project..." className="portfolio-image"/>
-                </div>
+                    <div className="portfolio-image-box">
+                        <img src={fallingstar} alt="falling star project..." className="portfolio-image"/>
+                        <div className="overflow"></div>
+                    </div>
                 {/* - */}
-                <div className="image-box-wrapper row justify-content-center">
-                    <img src={newpost} alt="newpost project..." className="portfolio-image"/>
-                </div>
+                    <div className="portfolio-image-box">
+                        <img src={newpost} alt="newpost project..." className="portfolio-image"/>
+                        <div className="overflow"></div>
+                    </div>    
                 {/* - */}
-                <div className="image-box-wrapper row justify-content-center">
-                    <img src={tapmate} alt="tapmate project..." className="portfolio-image"/>
+                    <div className="portfolio-image-box">
+                        <img src={tapmate} alt="tapmate project..." className="portfolio-image"/>
+                        <div className="overflow"></div>
+                    </div>    
                 </div>
             </div>
         </div>
