78d209d0b934450282da48876859f0bc50469706
Gonzalez32 <3232rose@gmail.com>
2021-05-17 15:42:57 -0500

    new component

---
+++ src/components/Contacts.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contacts = () => {
+    return (
+        <div className="contacts">
+            <h1 className="contact-me">Contact Me</h1>
+            <p>Please fill out the form and describe your project and I'll contect you as soon as possible.</p>
+            <p>Cheers.</p>
+        </div>
+    )
+}
+
+export default Contacts;
