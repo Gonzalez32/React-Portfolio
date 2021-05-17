bcc2fa6adab6de0a48cb2e7a6b8d9970b9d2ab8a
Gonzalez32 <3232rose@gmail.com>
2021-05-17 18:50:39 -0500

    modified 2 files

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -11,20 +11,25 @@ const Contacts = () => {
                 <div className="row">
                     <div className="col-md-6 col-xs-12">
                         {/* NAME INPUT */}
-                        <input id="name" className="form-control" placeholder="Name" name="name" type="text" />
+                        <input className="form-control" placeholder="Name" name="name" type="text" />
                         {/*  */}
+                        <div className="line"></div>
                         {/* PHONE INPUT */}
-                        <input id="phone" className="form-control" placeholder="Phone Number" phone="phone" type="phone" />
+                        <input className="form-control" placeholder="Phone Number" name="phone" type="phone" />
                         {/*  */}
+                        <div className="line"></div>
                         {/* EMAIL INPUT */}
-                        <input id="email" className="form-control" placeholder="Email" email="email" type="email" />
+                        <input className="form-control" placeholder="Email" name="email" type="email" />
                         {/*  */}
+                        <div className="line"></div>
                         {/* SUBJECT INPUT */}
-                        <input id="subject" className="form-control" placeholder="Subject" subject="subject" type="subject" />
+                        <input className="form-control" placeholder="Subject" name="subject" type="subject" />
                         {/*  */}
+                        <div className="line"></div>
                     </div>
                     <div className="col-md-6 col-xs-12">
-                        <textarea className="form-control" placeholder="Enter Text Here..." name="" id="description" cols="20" rows="6"></textarea>
+                        <textarea className="form-control" placeholder="Enter Text Here..." name="description" cols="20" ></textarea>
+                        <div className="line"></div>
                         <button className="btn-main-offer contact-btn" type="submit" >Contact Me</button>
                     </div>
                 </div>
