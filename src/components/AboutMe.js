2aaa7c6af19e74ae2306c08eca9eda8e795025d3
Gonzalez32 <3232rose@gmail.com>
2022-01-08 14:22:54 -0600

    modified aboutme - change layout

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -12,12 +12,13 @@ const AboutMe = () => {
                 </div>
                 <div className="col-lg-6 col-xm-12">
                     <h1 className="header-aboutme">About Me</h1>
+                    <br />
                     <p>
-                    A dedicated, optimistic, and self-driven full-stack software engineer that loves to learn
-                     and who tries to make the most out of difficult situations. This passion led me to challenge myself daily, 
-                     I'm not comfortable with settling and I'm always looking for an opportunity to do better and achieve greatness. 
-                     Ready to make a positive impact, join a great team and grow within. 
-                     With 6 years of machine operation services and project management experience and 1 year of web development experience.
+                        I am a full-stack engineer who tries to make the most out of difficult situations. 
+                        I'm not comfortable with settling and I’m always looking for opportunities to do better. 
+                        I still get nervous at daily stand-up and stand-down, but I enjoy these moments because we’re all together as a team.
+                        I am excited to solve, adept at learning quickly with a driven and positive attitude. 
+                        I look to find new creative ways to inspire and better the lives of others.
                     </p>
                 </div>
             </div>
