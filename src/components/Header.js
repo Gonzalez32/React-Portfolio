75e3f7c743d3f082e3c7d0e5fdeb4ede22ae2c67
@Juan <53915395+Gonzalez32@users.noreply.github.com>
2025-07-06 10:32:35 -0500

    updated header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
                 <h1>Juan Gonzalez portfolio</h1>
                 <Typed
                     className="typed-text"
-                    strings={["Production Support Engineer", "Skills =>", "Splunk", "DataGrip", "Analyzing code and other documents"]}
+                    strings={["Production Support Engineer", "Skills =>", "Splunk for IT operations", "DataGrip for database reporting", "AWS s3 bucket operations", "Analyzing Ruby code scripts and other documents", "Troubleshooting and assisting cross-functional teams"]}
                     typeSpeed={50}
                     backSpeed={60}
                     loop
